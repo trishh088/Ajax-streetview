7b5f9b3c5d8d99cbfcf49aa67f611a6cce2e3204
trishh088 <trishh088@gmail.com>
2017-08-14 14:49:08 +0530

    wiki and nyt ajax added

--- js/script.js
+++ js/script.js
@@ -22,7 +22,47 @@ function loadData() {
     $body.append('<img class="bgimg" src=" ' + streetviewUrl + '" >'); //adds the url image to the background
     // YOUR CODE GOES HERE!
 
-    return false;
+
+//NYT AJax request
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=4c3e33c2b2534f8c958d1c1e6084f75e';
+       $.getJSON(nytimesUrl,function(data){
+     $nytHeaderElem.text('New york Times articles about ' + cityStr);
+     articles = data.response.docs;
+     for(var i=0;i<articles.length;i++){
+       var article = articles[i];
+       $nytElem.append('<li class="article">'+
+           				'<a href="'+article.web_url+'">'+article.headline.main+
+           					'</a>'+
+           				'<p>' + article.snippet + '</p>'+
+           				'</li>');     };
+                }).fail(function(e) {
+              $nytHeaderElem.text('New york Times articles could not be loaded ');
+
+});
+
+
+//WIKIPEDIA AJax
+ var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+ var wikiRequestTimeout = setTimeout(function(){
+   $wikiElem.text("failed to get wiki resources");
+ },8000);
+
+ $.ajax({
+   url:wikiUrl,
+   dataType:"jsonp",
+   //jsonp:"callback",
+   success:function(response){
+     var articleList = response[1];
+     for(var i=0;i<articleList.length;i++){
+       articleStr=articleList[i];
+       var url='http://en.wikipedia.org/wiki/'+articleStr;
+       $wikiElem.append('<li><a href=" '+url+' " >'+ articleStr+ '</a></li>');
+     };
+     clearTimeout(wikiRequestTimeout);
+   }
+ });
+ return false; //important as you wont get the output without it
+
 };
 
 $('#form-container').submit(loadData); //calls the load data function
